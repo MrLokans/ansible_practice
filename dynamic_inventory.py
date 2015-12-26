68c7fe6db7410e0eb11ec32dda9f4c8f66a8971d
MrLokans <trikster1911@gmail.com>
2015-12-26 09:11:21 +0300

    Added host information parsing.

--- dynamic_inventory.py
+++ dynamic_inventory.py
@@ -16,6 +16,17 @@ import subprocess
 import paramiko
 
 
+def list_running_hosts():
+    cmd = ["vagrant", "status", "--machine-readable"]
+    status = subprocess.check_output(cmd).rstrip()
+    hosts = []
+    for line in status.split('\n'):
+        _, host, key, value = line.split(',')
+        if key == 'state' and value == 'running':
+            hosts.append(host)
+    return hosts
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Vagrant dynamic host information")
     group = parser.add_mutually_exclusive_group(required=True)
