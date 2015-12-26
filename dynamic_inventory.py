2e3a5b9fa924e5cada7b0c7736a0c6ce45e6053c
MrLokans <trikster1911@gmail.com>
2015-12-26 09:22:35 +0300

    CLI parser fixed. Script now may be run in python 2 and 3

--- dynamic_inventory.py
+++ dynamic_inventory.py
@@ -20,6 +20,8 @@ def list_running_hosts():
     cmd = ["vagrant", "status", "--machine-readable"]
     status = subprocess.check_output(cmd).rstrip()
     hosts = []
+    # print status
+    status = status.decode('UTF-8')
     for line in status.split('\n'):
         _, host, key, value = line.split(',')
         if key == 'state' and value == 'running':
@@ -44,13 +46,20 @@ def get_host_ssh_info(host):
 def parse_args():
     parser = argparse.ArgumentParser(description="Vagrant dynamic host information")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--list', action="store_true", description="List vagrant hosts information")
-    group.add_argument('--host', description="Show host information")
+    group.add_argument('--list', action="store_true")
+    group.add_argument('--host')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
+    if args.list:
+        hosts = list_running_hosts()
+        json.dump(hosts, sys.stdout)
+    else:
+        details = get_host_ssh_info(args.host)
+        json.dump(details, sys.stdout)
 
 
 if __name__ == '__main__':
+    main()
